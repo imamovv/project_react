bb46dbfc5a72eea4a2fe241ed09f6afe4156916a
imamovv <imamovv@gmail.com>
2024-05-30 22:43:18 +0300

    1

---
+++ src/components/App/App.tsx
@@ -0,0 +1,21 @@
+import {Outlet} from "react-router-dom";
+import MyHeader from "../MyHeader/MyHeader";
+import './App.css'
+
+import { Layout } from 'antd';
+
+const {Sider, Footer, Content } = Layout;
+
+function App() {
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+            <Sider width="auto" theme='light'><MyHeader/></Sider>
+            <Content><Outlet /></Content>
+            <Footer style={{color: 'black', background: '#ffffff'}}>
+                {new Date().getFullYear()}
+            </Footer>
+        </Layout>
+    );
+}
+
+export default App;
